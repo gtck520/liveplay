24cf87e7344704258b3a955bb030b8d41418ae7b
gtck520 <371609974@qq.com>
2024-03-26 14:03:15 +0800

    Add files via upload

---
+++ reader.py
@@ -0,0 +1,18 @@
+import redis
+import subprocess
+
+
+r = redis.StrictRedis(host='106.55.181.114', port=16379, db=0,password='dffg12dghj')
+    
+while True:
+    key, message = r.blpop('text_queue')
+    text = message.decode('utf-8')
+    import subprocess
+    voice = "zh-CN-YunxiNeural"
+
+    if text.strip():  # 检查文本是否为空（删除前后的空白字符后）
+        print("开始朗读"+text)
+        command = ["edge-playback", "--text", text, "--voice", voice]
+        subprocess.run(command)
+    else:
+        print("文本为空跳过")
---
+++ writer.py
@@ -0,0 +1,18 @@
+import redis
+
+# 连接到Redis服务器
+r = redis.StrictRedis(host='106.55.181.114', port=16379, db=0,password='dffg12dghj')
+print(r.ping())
+# 输入要朗读的文本
+text = input("请输入要朗读的文本：")
+
+# 将文本加入到队列中
+r.rpush('text_queue', text.encode('utf-8'))
+
+print("文本已添加到队列中。")
+
+# 获取当前队列数据
+queue_data = r.lrange('text_queue', 0, -1)
+print("当前队列数据：")
+for data in queue_data:
+    print(data.decode('utf-8'))
